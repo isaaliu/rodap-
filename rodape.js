fb9e86b61e76aba6a56b65f0018e4a4cefa9eb34
isaaliu <tiktoknctzen@gmail.com>
2025-06-18 14:38:34 -0300

    Add files via upload

---
+++ rodape.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./rodape.css";
+
+const Rodape = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 Meu Site. Todos os direitos reservados.</p>
+        </footer>
+    );
+};
+
+export default Rodape;
